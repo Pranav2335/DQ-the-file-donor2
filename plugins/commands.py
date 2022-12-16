8ccf55c4ad591d6f7bd36e84d9039078ea32cb8c
Pranav2335 <106325687+Pranav2335@users.noreply.github.com>
2022-12-16 19:45:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,10 +22,10 @@ BATCH_FILES = {}
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
-                    InlineKeyboardButton('⚡️Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ⚡️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('➕️Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ➕️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('⚔️Bᴏᴛ Oᴡɴᴇʀ⚔️', url=f"https://t.me/Owner_PM_Bot"),
-                    InlineKeyboardButton('💫Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ💫', url=GRP_LNK)
+                    InlineKeyboardButton('👑Bᴏᴛ Oᴡɴᴇʀ👑', url=f"https://t.me/Owner_PM_Bot"),
+                    InlineKeyboardButton('👁️‍🗨️Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ👁️‍🗨️', url=GRP_LNK)
                 ]] 
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
@@ -40,10 +40,10 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('⚡️Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ⚡️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('➕️Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ➕️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('⚔️Bᴏᴛ Oᴡɴᴇʀ⚔️', url=f"https://t.me/Owner_PM_Bot"),
-                    InlineKeyboardButton('💫Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ💫', url=GRP_LNK)
+                    InlineKeyboardButton('➕️Bᴏᴛ Oᴡɴᴇʀ➕️', url=f"https://t.me/Owner_PM_Bot"),
+                    InlineKeyboardButton('👁️‍🗨️Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ👁️‍🗨️', url=GRP_LNK)
                ]] 
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_sticker("CAACAgIAAxkBAAEGxuNjlYLKxvQob1xJxXHkR8XOJM6AbwACuA0AArqQ6Es7HbxOVjAxMysE")
@@ -86,10 +86,10 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-                    InlineKeyboardButton('⚡️Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ⚡️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('➕️Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ➕️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('⚔️Bᴏᴛ Oᴡɴᴇʀ⚔️', url=f"https://t.me/Owner_PM_Bot"),
-                    InlineKeyboardButton('💫Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ💫', url=GRP_LNK)
+                    InlineKeyboardButton('👑Bᴏᴛ Oᴡɴᴇʀ👑', url=f"https://t.me/Owner_PM_Bot"),
+                    InlineKeyboardButton('👁️‍🗨️Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ👁️‍🗨️', url=GRP_LNK)
                 ]] 
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_sticker("CAACAgIAAxkBAAEGxuNjlYLKxvQob1xJxXHkR8XOJM6AbwACuA0AArqQ6Es7HbxOVjAxMysE")
